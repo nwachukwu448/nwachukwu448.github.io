b9aa873cfefd8d395b947e6018533ac6d1c592ca
captain <nwachukwu448@gmail.com>
2025-06-06 13:37:28 +0100

    initial commit

---
+++ image/Images.jsx
@@ -0,0 +1,7 @@
+const Images ={
+    logo: new URL('./logo2.png', import.meta.url).href,
+    peter: new URL('./tech.jpg', import.meta.url).href,
+    caro1: new URL('./tech2.jpg', import.meta.url).href,
+    caro2: new URL('./tech5.jpg', import.meta.url).href
+}
+export default Images
